5a353c21f0449082e9954a77ba3949713abfb822
Beuty Isis <beutyisismail@gmail.com>
2019-10-26 01:31:30 +0700

    Add Services

---
+++ product/api.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class Product(Resource):
+   def get(self):
+      return {'asdf','asdf','asdf'}
+
+
+#create routes
+api.add_resource(Product, '/')
+
+
+# run the application
+if __name__ = '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
